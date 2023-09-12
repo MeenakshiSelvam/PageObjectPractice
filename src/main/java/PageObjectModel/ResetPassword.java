b9f4faa3c501eddd80d5cac0d2d4b3fabc0c0bb3
MeenakshiSelvam <sssmeena207@gmail.com>
2023-09-12 13:11:58 +0530

    page object

---
+++ src/main/java/PageObjectModel/ResetPassword.java
@@ -0,0 +1,36 @@
+package PageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ResetPassword {
+    WebDriver ldriver;
+    private static final By FORGOT_PASSWORD = By.className("oxd-text oxd-text--p orangehrm-login-forgot-header");
+    private static final By USERNAME = By.xpath("//input[@placeholder='Username']");
+    private static final By RESET = By.xpath("//button[normalize-space()='Reset Password']");
+
+    public ResetPassword(WebDriver rdriver) {
+        ldriver = rdriver;
+    }
+
+    public void clickForgotPassword()
+    {
+
+        ldriver.findElement(FORGOT_PASSWORD).click();
+    }
+    public void fillUsername(String username) {
+        ldriver.findElement(USERNAME).clear();
+        ldriver.findElement(USERNAME).sendKeys(username);
+    }
+
+    public void clickresetPassword() {
+        ldriver.findElement(RESET).click();
+    }
+
+
+
+}
+
+
+
+
